75098ca6c3ac280bf781194a8e0ac4f1cee87ac8
MSosa13 <matiasesosa1@gmail.com>
2026-06-19 15:33:08 -0300

    a

---
+++ a/Assets/interactable.cs
@@ -0,0 +1,35 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class interactable : MonoBehaviour
+{
+    public int contadorInicio = 60;
+    float contador;
+    bool countdown = false;
+
+    private void Start()
+    {
+        contador = contadorInicio;
+    }
+    void Update()
+    {
+
+        if (countdown)
+        {
+            contador -= Time.deltaTime;
+            Debug.Log("Tiempo restante: " + contador.ToString("F1"));
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            {
+                countdown = true;
+            }
+        }
+
+    }
+}
+
---
+++ a/Assets/movimiento.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public int Speed = 25;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontal = Input.GetAxisRaw("Horizontal");
+       float vertical = Input.GetAxisRaw("Vertical");
+
+       Vector2 movement = new Vector2(horizontal, vertical) * Speed * Time.deltaTime;
+        rb.linearVelocity = movement;
+    }
+}
